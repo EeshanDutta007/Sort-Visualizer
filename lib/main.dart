cd7910577c19dbd137da718123db3a38574868f4
Eeshan Dutta <eeshandutta21@gmail.com>
2020-10-24 10:34:36 +0530

    Proper animation of Sort

--- lib/main.dart
+++ lib/main.dart
@@ -26,26 +26,27 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+int samplesize = 10;
+
 class _MyHomePageState extends State<MyHomePage> {
   StreamController<List<int>> _streamController;
   Stream<List<int>> _stream;
   List<int> _numbers = [];
-  int _samplesize = 500;
+
   int counter = 0;
   void _randomize() {
     _numbers = [];
-    for (int i = 0; i < _samplesize; i++)
-      _numbers.add(Random().nextInt(_samplesize));
+    for (int i = 0; i < samplesize; i++) _numbers.add(Random().nextInt(500));
     _streamController.add(_numbers);
   }
 
   Future<void> _sort() async {
-    for (int i = 0; i < 500; i++) {
-      for (int j = 0; j < 500 - i - 1; j++) {
+    for (int i = 0; i < _numbers.length; i++) {
+      for (int j = 0; j < _numbers.length - i - 1; j++) {
         if (_numbers[j] > _numbers[j + 1])
           _numbers[j] =
               _numbers[j] + _numbers[j + 1] - (_numbers[j + 1] = _numbers[j]);
-        await Future.delayed(Duration(microseconds: 500));
+        await Future.delayed(Duration(microseconds: 100000));
         _streamController.add(_numbers);
       }
     }
@@ -61,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var a = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Bubble Sort')),
@@ -78,12 +80,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: numbers.map((int num) {
                   counter++;
                   return Container(
-                    child: CustomPaint(
-                      painter: BarPainter(
-                          width:
-                              MediaQuery.of(context).size.width / _samplesize,
+                    child: Padding(
+                      padding: const EdgeInsets.all(0.5),
+                      child: CustomPaint(
+                        painter: BarPainter(
+                          width: MediaQuery.of(context).size.width /
+                              (samplesize + 1),
                           value: num,
-                          index: counter),
+                          index: counter,
+                          a: a,
+                        ),
+                      ),
                     ),
                   );
                 }).toList(),
@@ -117,8 +124,8 @@ class BarPainter extends CustomPainter {
   final double width;
   final int value;
   final int index;
-
-  BarPainter({this.width, this.value, this.index});
+  var a;
+  BarPainter({this.width, this.value, this.index, this.a});
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint();
@@ -143,7 +150,7 @@ class BarPainter extends CustomPainter {
     } else {
       paint.color = Color(0xFF0E4D64);
     }
-    paint.strokeWidth = 5.0;
+    paint.strokeWidth = a / samplesize;
     paint.strokeCap = StrokeCap.square;
 
     canvas.drawLine(Offset(index * width, 0),
